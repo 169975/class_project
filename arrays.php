a5b6d893e07f824e5a653803bc01ea981b930221
169975 <daisy.gitonga@strathmore.edu>
2024-05-24 12:18:01 +0300

    I have created a loop php file

---
+++ arrays.php
@@ -0,0 +1,58 @@
+<?php
+//indexed or numeric array
+$colors = ["Black" , "Green", "Yellow"];
+
+print_r($colors);
+
+?>
+<br>
+
+<?php
+$user= array("Alex","Peter","Ann");
+?>
+
+<pre>
+   <?php print_r($user[2]);?>
+</pre>
+
+
+<?php
+//associative arrays
+$user_data=[
+    "fullname" =>"Alex Okama",
+    "email" => "AOkama@yahoo.com",
+    "phone" =>"+2548458965"
+    ];
+print $user_data["email"];
+    ?>
+    <pre>
+        <?php print_r($user_data);?>
+  
+    </pre>
+
+    <?php
+    //multidimensional arrays
+
+    $user_details=[
+    "Director" => array(
+    "fullname" =>"Alex Okama",
+    "address"=>"Mada",
+    "email" => "AOkama@yahoo.com",
+    "phone" =>[
+        "Home" => "+2548458565",
+        "Work"
+
+    ]
+    "+2548458965"
+    ]
+        ),
+    ?>
+
+
+
+
+
+
+
+print $user_details
+["Secretary"]["phone"]
